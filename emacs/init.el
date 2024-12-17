524c7695ccbd17e6413bf6bcd9af4d680cb4326f
Arne Magnus Tveita Løken <117858954+amtloken@users.noreply.github.com>
2024-12-17 08:18:05 +0100

    Restrukturering med init heller enn early-init

---
+++ emacs/init.el
@@ -0,0 +1,10 @@
+;; Configure package.el to include MELPA.
+(require 'package)
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.prg/packages/") t)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/packages/") t)
+(package-initialize)
+
+(defun package--save-selected-packages (&rest opt) nil)
+
+(org-babel-load-file (concat user-emacs-directory "configuration.org"))
